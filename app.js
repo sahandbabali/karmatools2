944d9387bfecf8ed51ef75c24e2475d539ca5f8d
sahandbabali <sahand.babali@gmail.com>
2021-09-12 00:44:37 +0430

    back to top button 2

--- app.js
+++ app.js
@@ -158,7 +158,6 @@ mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
-  console.log("scrolling...");
   scrollFunction();
 };
 
